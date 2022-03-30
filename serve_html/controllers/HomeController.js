48cf0968eca479d7a1707b84a0e9dbbcf6d4e365
CR0416 <karinachen1314@yahoo.com>
2022-03-29 23:42:49 -0400

    first commit

---
+++ serve_html/controllers/HomeController.js
@@ -0,0 +1,34 @@
+//handle errors
+const httpStatus = require("http-status-codes");
+
+exports.respondNoResourceFound = (req, res) => {
+    let errorCode = httpStatus.StatusCodes.NOT_FOUND;
+    res.status(errorCode);
+    res.send(`${errorCode} | The page requested does not exist!`);
+};
+
+exports.respondInternalError =(error, req, res, next) => {
+    let errorCode= httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occured: ${error.stack}`)
+    res.status(errorCode);
+    res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+};
+
+//log the URL acessed
+exports.logRequestPaths =(req, res, next) => {
+    console.log(`request made to: ${req.url}`);
+    next();
+};
+//home
+exports.sendReqParam = (req, res) => {
+    let home= req.params.home;
+    res.render('home', {layout: 'home'})
+};
+
+//images/x
+exports.respondwithimage= (req, res) => {
+    let paramimg = req.params.image;
+    res.render("picture",{layout: "picture"});
+    res.render("picture", {name: paramimg});
+};
+
---
+++ serve_html/index.js
@@ -0,0 +1,34 @@
+const express= require("express"),
+    app = express()
+    homeController = require("./controllers/HomeController");
+    layouts = require("express-ejs-layouts")
+    path = require("path")
+
+app.set("port", process.env.PORT || 2000);
+app.set("view engine", "ejs");
+app.use(express.static("views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(layouts);
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.json());
+app.use(homeController.logRequestPaths);
+
+app.get("/home", homeController.sendReqParam);
+app.get("/images/:image", homeController.respondwithimage);
+
+app.post("/"), (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res.send("POST Sucessful!")
+};
+
+app.use(homeController.respondNoResourceFound);
+app.use(homeController.respondInternalError);
+app.listen( app.get("port"), () => {
+    console.log(`The server has started at http://localhost:${app.get("port")}`);
+});
